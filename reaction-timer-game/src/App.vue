3e941a005760ec6c2f2aa004d2d3b45febc55475
habiburahmanrulialif <habibalif865@gmail.com>
2024-06-06 09:43:27 +0700

    setup done

---
+++ reaction-timer-game/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div>Reaction Timer</div>
+
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
