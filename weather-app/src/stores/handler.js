a4a0659efa58ad1e273f8b175716faa737eb881f
habiburahmanrulialif <habibalif865@gmail.com>
2024-06-16 21:46:42 +0700

    searchbar with geocoding done, next consume weather api

--- weather-app/src/stores/handler.js
+++ weather-app/src/stores/handler.js
@@ -1,5 +1,7 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import axios from 'axios'
+import { errorMessages } from 'vue/compiler-sfc';
 
 export const handlerStore = defineStore('handler', () => {
   const modalActive = ref(false)
@@ -8,4 +10,34 @@ export const handlerStore = defineStore('handler', () => {
     modalActive.value = !modalActive.value;
   }
   return { modalActive, modalhandler }
+});
+
+
+export const mapSearch = defineStore('map', () => {
+  const queryTimeout = ref(null);
+  const mapboxApiKey = "pk.eyJ1IjoidGVyaXR1bCIsImEiOiJjbHhnNmVqZmExMGpnMm1vYXd1ZmcyaGtyIn0.tQX38PvSa9JF9PwJvPfTYA";
+  const mapboxSearchResults = ref(null)
+  const searchError = ref(null)
+
+
+  const getSearchResults = (query) => {
+    clearTimeout(queryTimeout.value);
+    queryTimeout.value = setTimeout(async() => {
+      if (query !== '') {
+        try {
+          const result = await axios.get(`https://api.mapbox.com/search/geocode/v6/forward?q=${query}&access_token=${mapboxApiKey}`);
+          mapboxSearchResults.value = result.data.features;
+          console.log(mapboxSearchResults.value)
+        }
+        catch
+        {
+          searchError.value = true
+        }
+        return;
+      };
+      mapboxSearchResults.value = null;
+    }, 300)
+  }
+
+  return { mapboxSearchResults, searchError, getSearchResults }
 })
--- weather-app/src/views/HomeView.vue
+++ weather-app/src/views/HomeView.vue
@@ -1,7 +1,39 @@
-<script setup>
-</script>
-
 <template>
-  <main>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input type="text" v-model="searchQuery" @input="getResult" placeholder="Masukkan daerah yang akang dicari"
+        class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]">
+      <ul class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+        v-if="search.mapboxSearchResults && searchQuery">
+        <p class="py-2" v-if="search.searchError">
+          Sorry, something went wrong, please try again.
+        </p>
+        <p class="py-2" v-if="!search.searchError && search.mapboxSearchResults.length === 0">
+          No results match your query, try a different term.
+        </p>
+        <template v-else>
+          <li v-for="searchResult in search.mapboxSearchResults" :key="searchResult.id"
+            class="py-2 cursor-pointer text-white">
+            {{ searchResult.properties.full_address }}
+          </li>
+        </template>
+      </ul>
+    </div>
   </main>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+import { mapSearch } from '../stores/handler.js';
+
+const searchQuery = ref("")
+
+const search = mapSearch()
+
+const getResult = () => {
+  if (searchQuery.value != "") {
+    search.getSearchResults(searchQuery.value)
+  }
+
+}
+</script>
